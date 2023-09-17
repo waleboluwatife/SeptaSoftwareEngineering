a495e42e74533af21aeadfc33f992a77d992d7d0
Michael <waleboluwatife@gmail.com>
2023-09-17 15:37:02 +0100

    Multiply and Subtract

---
+++ mult and Subt.js
@@ -0,0 +1,11 @@
+function twoNum(a, b) {
+    multiply = a * b;
+    console.log( a, '*', b, '=', sum );
+}
+twoNum(2, 1);
+
+function twoNum(a, b) {
+    sum = a - b;
+    console.log( a, '+', b, '=', sum );
+}
+twoNum(2, 1);
